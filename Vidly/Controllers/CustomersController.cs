7c76b35b1e3fc7f6e28890311b6a7cd20c8132c0
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-09-20 03:35:55 -0300

    Add validation summary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -35,7 +35,11 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new CustomerFormViewModel { MembershipTypes = membershipTypes };
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = new Customer(),
+                MembershipTypes = membershipTypes 
+            };
 
             return View("CustomerForm" ,viewModel);
         }
