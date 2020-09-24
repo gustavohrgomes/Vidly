040a0566ee0fc47fa4aff29218b464c6ce6a6479
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-09-23 22:52:23 -0300

    Create Movies API

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        // GET /api/movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        // POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movie);
+        }
+
+        // PUT /api/movies/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDB = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDB == null)
+                return NotFound();
+
+            movieInDB.Name = movie.Name;
+            movieInDB.ReleaseDate = movie.ReleaseDate;
+            movieInDB.GenreId = movie.GenreId;
+            movieInDB.InStock = movie.InStock;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE /api/movies/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.Single(m => m.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
