ab8016ff349ab36a28602da82bf95701299e8b63
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-05 21:01:25 -0300

    Building an API for renting movies

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDTO newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
