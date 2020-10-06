a042b80cbf21abb50b4954ac3fdae4a4321eb63c
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-05 22:30:02 -0300

    Update NewRentalsController API for validate the Number of movies available

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -21,13 +21,18 @@ namespace Vidly.Controllers.Api
         }
 
         [HttpPost]
-        public IHttpActionResult CreateNewRental(NewRentalDTO newRental)
+        public IHttpActionResult CreateNewRentals(NewRentalDTO newRental)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
             var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
+                movie.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
